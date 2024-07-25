6fa61705e31cde4e4a81c506fe705c43597639fa
davit1801 <155184127+davit1801@users.noreply.github.com>
2024-07-25 23:47:30 +0400

    adding sections

--- index.js
+++ index.js
@@ -28,6 +28,16 @@ function checkShowList() {
   );
 }
 
+//Clsoe dropdown on Click outside header navigation
+const headerNav = document.querySelector('.header-nav-items');
+document.addEventListener('click', (event) => {
+  if (!headerNav.contains(event.target)) {
+    headerNavItems.forEach((item) => {
+      item.classList.remove('show-list');
+      header.classList.remove('active');
+    });
+  }
+});
 
 //Header Mobile Navigation
 const mobNavItems = document.querySelectorAll('.mob-nav-item');
@@ -45,4 +55,49 @@ mobNavItems.forEach((item) => {
 const headerButton = document.querySelector('.mob-header-btn');
 headerButton.addEventListener('click', () => {
   header.classList.toggle('open-mob-nav');
+});
+
+// Footer accordion
+const footerDropdownToggles = document.querySelectorAll(
+  '.footer-dropdown-toggle'
+);
+footerDropdownToggles.forEach((item) => {
+  item.addEventListener('click', () => {
+    item.classList.toggle('show-nav');
+    footerDropdownToggles.forEach((otherItem) => {
+      if (otherItem !== item) {
+        otherItem.classList.remove('show-nav');
+      }
+    });
+  });
+});
+
+const wrapper = document.querySelector('.swipper-container');
+
+let pressed = false;
+let startX = 0;
+
+wrapper.addEventListener('mousedown', (e) => {
+  pressed = true;
+  startX = e.clientX;
+  wrapper.style.cursor = 'grabbing';
+
+  console.log(startX);
+});
+
+wrapper.addEventListener('mouseleave', (e) => {
+  pressed = false;
+});
+
+window.addEventListener('mouseup', (e) => {
+  pressed = false;
+  wrapper.style.cursor = 'grab';
+});
+
+wrapper.addEventListener('mousemove', (e) => {
+  if (!pressed) {
+    return;
+  }
+  // wrapper.scrollLeft += startX - e.clientX
+  wrapper.style.transform = 'translate3d(-100px, 0px, 0px)';
 });
